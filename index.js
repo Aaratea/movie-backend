367789aea87ceefa459c49fdce803b6af4fcfd34
Aaratea <aaratithapa439@gmail.com>
2023-07-11 12:25:29 +0545

    Upadate

--- index.js
+++ index.js
@@ -15,23 +15,19 @@ app.use(cookieParser());
 
 app.use("/api/v1", routes);
 
+const port = 4000;
 
 const server = http.createServer(app);
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-  .then(() => {
-    console.log('Connected to MongoDB');
-    // Additional code after successful connection
-  })
-  .catch((error) => {
-    console.error('Error connecting to MongoDB:', error);
-  });
 
-// Access other environment variables
-const port = process.env.PORT;
-const tokenSecretKey = process.env.TOKEN_SECRET_KEY;
-const tmdbBaseUrl = process.env.TMDB_BASE_URL;
-const tmdbKey = process.env.TMDB_KEY;
-//test
+const mongodbURI = "mongodb+srv://Aaratea:iamtheone12@cluster0.dofxhxb.mongodb.net/";
+
+mongoose.connect(mongodbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    server.listen(port, () => {
+      console.log(`Server is listening on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Failed to connect to MongoDB:", err);
+    process.exit(1);
+  });

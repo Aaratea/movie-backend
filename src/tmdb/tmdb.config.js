f13a0844efe94c047aeef6911c60a5c95710e989
Aaratea <aaratithapa439@gmail.com>
2023-07-11 12:27:44 +0545

    change

--- src/tmdb/tmdb.config.js
+++ src/tmdb/tmdb.config.js
@@ -1,10 +1,9 @@
-const baseUrl = process.env.TMDB_BASE_URL;
 const key = process.env.TMDB_KEY;
 
 const getUrl = (endpoint, params) => {
   const qs = new URLSearchParams(params);
 
-  return `${baseUrl}${endpoint}?api_key=${key}&${qs}`;
+  return `https://api.themoviedb.org/3/${endpoint}?api_key=6785b1695a6dcaadd5f07764d099b2c0&${qs}`;
 };
 
 export default { getUrl };
